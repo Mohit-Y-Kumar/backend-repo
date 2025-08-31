ec5256cc10f8cf43d0b166547c5412e511823298
Mohit-Y-Kumar <my1362321@gmail.com>
2025-08-31 17:43:59 +0530

    update  server  setup

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ require('./config/passport')(passport);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(passport.initialize());
 
 app.use('/auth', authRoutes);
 app.use('/notes', notesRoutes);
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+
 
 app.use((err, req, res, next) => {
   console.error(err);
