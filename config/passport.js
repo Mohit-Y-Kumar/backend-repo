dbcb5af316677d256970002efd319cbae2297029
Mohit-Y-Kumar <my1362321@gmail.com>
2025-08-31 13:20:49 +0530

    Add passport configuration for authentication

---
+++ config/passport.js
@@ -0,0 +1,28 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+module.exports = function(passport) {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+  }, async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      if (user) return done(null, user);
+      // if email exists but not googleId, you might want to link accounts
+      user = new User({
+        googleId: profile.id,
+        name: profile.displayName,
+        email: profile.emails && profile.emails[0] && profile.emails[0].value,
+      });
+      await user.save();
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  }));
+  console.log("Client ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("Callback URL:", process.env.GOOGLE_CALLBACK_URL);
+
+};
