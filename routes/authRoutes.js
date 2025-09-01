7af0e0fe8945ccc2339aca6fe1f548f178cd3979
Mohit-Y-Kumar <my1362321@gmail.com>
2025-09-01 12:18:15 +0530

    update google route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -81,7 +81,7 @@ router.post('/verify-otp', async (req, res, next) => {
   }
 });
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', passport.authenticate('google', { scope: ['openid','profile', 'email'] }));
 
 router.get('/google/callback',
   passport.authenticate('google', { session: false, failureRedirect: '/auth/google/failure' }),
